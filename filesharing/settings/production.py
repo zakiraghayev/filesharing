50da10d1d4c3c7234600a2e63c33a4388818f4d2
zakiraghayev <zakir.aghayev.1999@gmail.com>
2021-07-13 10:09:46 +0400

    Static files config

--- filesharing/settings/production.py
+++ filesharing/settings/production.py
@@ -135,10 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static/')
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 MEDIA_URL = '/media/'
-MEDIA_ROOT =  os.path.join(os.path.dirname(BASE_DIR),'media/')
+MEDIA_ROOT =  os.path.join(BASE_DIR,'media/')
 
 # Registeration Conf
 ACCOUNT_ACTIVATION_DAYS = 7
